49828ae89234ada73ff01cf88c0dda34222e2205
maingh-dev <singh.manish7@outlook.in>
2021-12-29 23:10:57 +0530

    Add Spring Boot Book Seller Web Application

---
+++ src/main/java/org/book/springbootbookseller/SpringBootBookSellerApplication.java
@@ -0,0 +1,13 @@
+package org.book.springbootbookseller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBookSellerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBookSellerApplication.class, args);
+	}
+
+}
