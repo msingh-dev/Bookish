3f020b40f854a40eac44242ca1fed157670c51c3
maingh-dev <singh.manish7@outlook.in>
2021-12-30 15:30:34 +0530

    Add Purchase Model Class

---
+++ src/main/java/org/book/springbootbookseller/model/PurchaseHistory.java
@@ -0,0 +1,27 @@
+package org.book.springbootbookseller.model;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "purchase_history")
+public class PurchaseHistory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    @Column(name = "book_id", nullable = false)
+    private Long bookId;
+
+    @Column(name = "price", nullable = false)
+    private Double price;
+
+    @Column(name = "purchase_time", nullable = false)
+    private LocalDateTime purchaseTime;
+
+
+}
