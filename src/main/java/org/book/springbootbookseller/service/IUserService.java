d9425423ab92ccf2b147fafcf3622ee2d8434978
maingh-dev <singh.manish7@outlook.in>
2021-12-30 20:36:33 +0530

    Add service Layer class to the Application

---
+++ src/main/java/org/book/springbootbookseller/service/IUserService.java
@@ -0,0 +1,13 @@
+package org.book.springbootbookseller.service;
+
+import org.book.springbootbookseller.model.User;
+
+import java.util.Optional;
+
+public interface IUserService {
+    User saveUser(User user);
+
+    Optional<User> findByUsername(String username);
+
+    void makeAdmin(String username);
+}
