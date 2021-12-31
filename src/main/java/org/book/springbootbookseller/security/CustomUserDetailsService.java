1cd0bc8487ecef647020c9a3f8a91756a8200739
maingh-dev <singh.manish7@outlook.in>
2021-12-31 13:45:33 +0530

    Add Security layer Configurations

---
+++ src/main/java/org/book/springbootbookseller/security/CustomUserDetailsService.java
@@ -0,0 +1,39 @@
+package org.book.springbootbookseller.security;
+
+import org.book.springbootbookseller.model.User;
+import org.book.springbootbookseller.service.UserService;
+import org.book.springbootbookseller.util.SecurityUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserService userService;
+
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userService.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException(username));
+
+        Set<GrantedAuthority> authorities = Set.of(SecurityUtils.convertToAuthority(user.getRole().name()));
+
+        return UserPrincipal.builder()
+                .user(user).id(user.getId())
+                .username(username)
+                .password(((User) user).getPassword())
+                .authorities(authorities)
+                .build();
+    }
+}
---
+++ src/main/java/org/book/springbootbookseller/security/UserPrincipal.java
@@ -0,0 +1,62 @@
+package org.book.springbootbookseller.security;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.book.springbootbookseller.model.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Set;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserPrincipal implements UserDetails {
+
+    private Long id;
+    private String username;
+    transient private String password; //dont show up on serialized operations
+    transient private User user; // use for login operation, dont use it in JWT.
+    private Set<GrantedAuthority> authorities;
+
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
